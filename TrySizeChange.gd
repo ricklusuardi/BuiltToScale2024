1612f55b0cd57f282e952a74f546c0b885556df6
BlackBarman <74059223+BlackBarman@users.noreply.github.com>
2024-08-17 14:25:52 +0200

    kkkkkk
    tileset and other stuff pushed

---
+++ TrySizeChange.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	if Input.is_action_just_pressed("ui_character_size_increase"):
+		
+		if (scale < Vector2(8,8)):
+			scale *= 2
+
+	if Input.is_action_just_pressed("ui_character_size_decrease"):
+		if (scale > Vector2(0.25, 0.25)):
+			scale /= 2
+	
+	pass
+
