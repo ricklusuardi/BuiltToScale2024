6dbbd199db9965dfd914c7d2f536a1f62a662588
BlackBarman <74059223+BlackBarman@users.noreply.github.com>
2024-08-18 15:39:17 +0200

    fineshed the wind mechanich

--- Chara.gd
+++ Chara.gd
@@ -2,27 +2,26 @@ extends CharacterBody2D
 class_name Chara
 
 var SPEED = 200.0
-#const JUMP_VELOCITY = -400.0
-
-
-#SCALE
-@export var scaleChange : Vector2 #how much the size of the character increases/decreases
-@export var currentScaleStep : int = 0 # keeps track of how many times the character has been scaled up or down. 
-@export var maxScaleUp : int = 2 # max amount of times it can be scaled up
-@export var minScaleDown : int = - 2 # max amount of times it can be scaled down
-var is_in_wind = false
 @export var in_wind_speed = 50
+var is_in_wind = false
+var wind_direction = Vector2.ZERO
 
-#MASS
+# SCALE
+@export var scaleChange: Vector2  # how much the size of the character increases/decreases
+@export var currentScaleStep: int = 0  # keeps track of how many times the character has been scaled up or down
+@export var maxScaleUp: int = 2  # max amount of times it can be scaled up
+@export var minScaleDown: int = -2  # max amount of times it can be scaled down
+
+# MASS
 var baseMass = 1
 var currentMass = baseMass
-@export var massChange : float #how much the mass of the character increases/decreases
+@export var massChange: float  # how much the mass of the character increases/decreases
 
-#ANIMATION
-var animRunMultiplier = 3 #how much quicker the animation should be when running
+# ANIMATION
+var animRunMultiplier = 3  # how much quicker the animation should be when running
 var animIdleMultiplier = 1
 var currentAnimScale = 1
-var animScaleMultiplier = 1.5 #how much quicker/slower the animations should be when sizes change
+var animScaleMultiplier = 1.5  # how much quicker/slower the animations should be when sizes change
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -31,65 +30,87 @@ func _ready():
 	randomize()
 
 func _physics_process(delta):
+	print(currentMass)
 	if is_in_wind:
-		SPEED = in_wind_speed 
-	else :
+		apply_wind_effect(delta)
+	else:
 		SPEED = 200.0
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	#MOVEMENT
+	# MOVEMENT
 	if direction:
 		velocity.x = direction * SPEED
 		$AnimationPlayer.play("RobotRun")
-		$AnimationPlayer.speed_scale =  animRunMultiplier * currentAnimScale
-		#$AudioStreamPlayer.pitch_scale = randf_range(0.9, 1.1)
+		$AnimationPlayer.speed_scale = animRunMultiplier * currentAnimScale
 		if velocity.x > 0:
 			$Sprite2D.flip_h = false
 		elif velocity.x < 0:
 			$Sprite2D.flip_h = true
 	else:
-		#IDLE
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		# If standing still, let the wind move the character
+		if is_in_wind:
+			velocity.x += (wind_direction.x * (in_wind_speed/currentMass) * delta)
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		
 		$AnimationPlayer.play("RobotIdle")
 		$AnimationPlayer.speed_scale = animIdleMultiplier * currentAnimScale
-		
-		
+
 	if Input.is_action_just_pressed("ui_increase_size"):
 		_increase_size()
 	if Input.is_action_just_pressed("ui_decrease_size"):
 		_decrease_size()
 
+	# Process movement
 	move_and_slide()
 
 func _increase_size():
-	if (currentScaleStep < maxScaleUp):
+	if currentScaleStep < maxScaleUp:
 		scale *= scaleChange
 		currentMass *= massChange
 		currentScaleStep += 1
-		currentAnimScale /= animScaleMultiplier #anim is slower when char is bigger
+		currentAnimScale /= animScaleMultiplier  # anim is slower when char is bigger
 		$AudioStreamPlayerSizeDown.pitch_scale -= 0.2
 		$AudioStreamPlayerSizeUp.pitch_scale -= 0.2
 		$AudioStreamPlayerSizeUp.play()
 
 func _decrease_size():
-	if (currentScaleStep > minScaleDown):
+	if currentScaleStep > minScaleDown:
 		scale /= scaleChange
 		currentMass /= massChange
 		currentScaleStep -= 1
-		currentAnimScale *= animScaleMultiplier #anim is faster when char is smaller
+		currentAnimScale *= animScaleMultiplier  # anim is faster when char is smaller
 		$AudioStreamPlayerSizeDown.pitch_scale += 0.2
 		$AudioStreamPlayerSizeUp.pitch_scale += 0.2
 		$AudioStreamPlayerSizeDown.play()
 
-func in_wind():
-	is_in_wind = true
+# Adjust the speed based on the wind direction and character movement
+func apply_wind_effect(delta: float):
+	var direction = Input.get_axis("ui_left", "ui_right")
 
+	if direction != 0:
+		if (direction > 0 and wind_direction == Vector2.LEFT) or (direction < 0 and wind_direction == Vector2.RIGHT):
+			# Slow down against the wind
+			SPEED = max(SPEED - (in_wind_speed/currentMass), 50)
+		elif (direction < 0 and wind_direction == Vector2.LEFT) or (direction > 0 and wind_direction == Vector2.RIGHT):
+			# Speed up with the wind
+			SPEED = min(SPEED + (in_wind_speed/currentMass), 400)
+	else:
+		# If standing still, the wind effect is handled in _physics_process
+		pass
+
+# Called when the character enters the wind area
+func in_wind(wind_vector: Vector2):
+	is_in_wind = true
+	wind_direction = wind_vector
+
+# Called when the character exits the wind area
 func not_in_wind():
-	is_in_wind =  false
-		
+	is_in_wind = false
+	wind_direction = Vector2.ZERO
--- WindArea.gd
+++ WindArea.gd
@@ -1,24 +1,33 @@
 extends Area2D
 class_name WindArea
 
+@onready var character: Chara = %Character
 
+# Define the enum for wind direction
+enum WindDirection {
+	LEFT,
+	RIGHT
+}
 
-@onready var character :Chara = %Character
+@export var wind_direction: WindDirection = WindDirection.LEFT  # The direction of the wind
+#@export var wind_force: float = 50.0  # The force of the wind
 
-func _ready():
-	connect("body_entered", _on_body_entered)
-	connect("body_exited", _on_body_exited)
-	
+# Returns the wind direction as a Vector2
+func get_wind_vector() -> Vector2:
+	match wind_direction:
+		WindDirection.LEFT:
+			return Vector2.LEFT
+		WindDirection.RIGHT:
+			return Vector2.RIGHT
+	return Vector2.ZERO
 
+# Called when a body enters the wind area
 func _on_body_entered(body):
-	if body == character :
-		character.in_wind()
-		#TODO usare velocity
-		print("ciao")
+	if body == character:
+		var wind_vector = get_wind_vector()
+		character.in_wind(wind_vector)
 
-
+# Called when a body exits the wind area
 func _on_body_exited(body):
-	if body == character :
-		#TODO usare velocity
+	if body == character:
 		character.not_in_wind()
-		
