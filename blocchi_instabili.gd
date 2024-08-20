fcbfecc689e748a5614f352ab44ddc3f454efe5d
BlackBarman <74059223+BlackBarman@users.noreply.github.com>
2024-08-20 10:46:29 +0200

    perfected instable blocks and added them to the level ingredient tilemap

--- blocchi_instabili.gd
+++ blocchi_instabili.gd
@@ -1,16 +1,24 @@
 extends Node2D
 
 @onready var breaking_point := 1
+var is_block_alive = true
 
 func _on_blocchi_instabili_body_entered(body):
 	if body is Chara:
 		if body.currentMass == breaking_point:
-			self.queue_free()
-			#maybe add a animation?
+			print("ciao")
+			%AnimationPlayer.play("breaking_tile")
+
 
 
 func _on_blocchi_instabili_body_exited(body):
-		if body is Chara:
-			#stop animation
-			pass
+		if body is Chara and is_block_alive== true :
+			%AnimationPlayer.stop()
 
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "breaking_tile" :
+		is_block_alive = false
+		print("animation done")
+		queue_free()
+	pass # Replace with function body.
